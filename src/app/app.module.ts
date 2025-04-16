9efe2a8b2409394898e6b215255aaa313a3b93e2
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-04-16 17:02:26 +0200

    Upload thumbnail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,40 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {UploadVideoComponent} from './upload-video/upload-video.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {NgxFileDropModule} from "ngx-file-drop";
-import {MatButtonModule} from "@angular/material/button";
-import {MatToolbarModule} from "@angular/material/toolbar";
-import {MatIconModule} from "@angular/material/icon";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatSelectModule} from "@angular/material/select";
-import {MatOptionModule} from "@angular/material/core";
-import {MatInputModule} from "@angular/material/input";
-import {MatChipsModule} from "@angular/material/chips";
-import {MatSnackBarModule} from "@angular/material/snack-bar";
-import {MatSidenavModule} from "@angular/material/sidenav";
-import {MatListModule} from "@angular/material/list";
-import {MatCardModule} from "@angular/material/card";
-import {MatMenuModule} from "@angular/material/menu";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UploadVideoComponent } from './upload-video/upload-video.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { NgxFileDropModule } from 'ngx-file-drop';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 import { HeaderComponent } from './header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { SaveVideoDetailsComponent } from './save-video-details/save-video-details.component';
+import { VgCoreModule } from '@videogular/ngx-videogular/core';
+import { VgControlsModule } from '@videogular/ngx-videogular/controls';
+import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
+import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
 
 @NgModule({
   declarations: [
     AppComponent,
     UploadVideoComponent,
     HeaderComponent,
-    SaveVideoDetailsComponent
+    SaveVideoDetailsComponent,
   ],
   imports: [
     BrowserModule,
@@ -48,12 +53,17 @@ import { SaveVideoDetailsComponent } from './save-video-details/save-video-detai
     MatSidenavModule,
     MatListModule,
     MatCardModule,
-    MatMenuModule
+    MatMenuModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    FlexLayoutModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
   ],
-  providers: [
-    
-  ],
-  bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/video.service.ts
+++ src/app/video.service.ts
@@ -15,7 +15,17 @@ export class VideoService {
     const formData = new FormData()
     formData.append('file', fileEntry, fileEntry.name);
 
-    // HTTP Post call to upload the video
     return this.httpClient.post<UploadVideoResponse>("http://localhost:8080/api/videos", formData);
+  }
+
+  uploadThumbnail(fileEntry: File, videoId: string): Observable<string> {
+    const formData = new FormData()
+    formData.append('file', fileEntry, fileEntry.name);
+    formData.append('videoId', videoId);
+    console.log("uploadThumb")
+    // HTTP Post call to upload the thumbnail / the response is the url (String)
+    return this.httpClient.post("http://localhost:8080/api/videos/thumbnail", formData, {
+      responseType: 'text'
+    });
   }
 }
