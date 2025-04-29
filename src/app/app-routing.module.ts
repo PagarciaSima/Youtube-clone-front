a5d5c7a5d761b5bcbd1862ed1758a01771afe419
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-04-29 21:23:00 +0200

    Sidebar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,38 @@ import { RouterModule, Routes } from '@angular/router';
 import { UploadVideoComponent } from './upload-video/upload-video.component';
 import { SaveVideoDetailsComponent } from './save-video-details/save-video-details.component';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
+import { HomeComponent } from './home/home.component';
+import { LikedVideosComponent } from './liked-videos/liked-videos.component';
+import { HistoryComponent } from './history/history.component';
+import { SubscriptionsComponent } from './subscriptions/subscriptions.component';
 
 const routes: Routes = [
   {
-    path: 'upload-video', component: UploadVideoComponent
+    path: '', component: HomeComponent,
+    children: [
+
+      {
+        path: 'subscriptions', component: SubscriptionsComponent,
+      },
+      {
+        path: 'history', component: HistoryComponent,
+      },
+      {
+        path: 'liked-videos', component: LikedVideosComponent,
+      },
+    ]
+  },
+
+  {
+    path: 'upload-video', component: UploadVideoComponent,
   },
   {
-    path: 'save-video-details/:videoId', component: SaveVideoDetailsComponent
+    path: 'save-video-details/:videoId', component: SaveVideoDetailsComponent,
   },
   {
-    path: 'video-details/:videoId', component: VideoDetailComponent
+    path: 'video-details/:videoId', component: VideoDetailComponent,
   }
+
 ];
 
 @NgModule({
